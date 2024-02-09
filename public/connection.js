3ae8a7ba0ff7006e9c94efdf39d9ebbf134a548b
Deep-Kumawat <deepdkmail@gmail.com>
2024-02-09 07:05:21 +0530

    messages now show up on every open window

--- public/connection.js
+++ public/connection.js
@@ -1,23 +1,61 @@
-// let socket = io();
-// console.log(socket);
-// let form = document.getElementById('chatForm');
-// let messageBoxValue = document.getElementById('message_box').value;
+let socket = io();
+let form = document.getElementById('chatForm');
+let input = document.getElementById('message_box');
 
-// form.addEventListener('submit', (e)=>{
-//     e.preventDefault();
-//     socket.emit('message', messageBoxValue);
-// });
+function displayMessage(message){
+    socket.emit('removeTypingFeedback');
+    const chats = document.getElementById('chats');
+    const messageLi = document.createElement('li');
+    const messageLiTextNode = document.createTextNode(message);
+    messageLi.appendChild(messageLiTextNode);
+    chats.appendChild(messageLi);
+    // chats.classList.add('rightMessage');
+}
 
+function showTyping(){
+    const typingDiv = document.getElementById('typingTextDiv');
+    typingDiv.style.visibility = 'visible';
+}
 
-var socket = io();
+function hideTyping(){
+    const typingDiv = document.getElementById('typingTextDiv');
+    typingDiv.style.visibility = 'hidden';
+}
 
-  var form = document.getElementById('chatForm');
-  var input = document.getElementById('message_box');
-
-  form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (input.value) {
-      socket.emit('message', input.value);
-      input.value = '';
-    }
-  });
+        displayMessage(input.value);
+        socket.emit('message', input.value);
+        input.value = '';
+}
+}); 
+
+input.addEventListener('focus', function(e) {
+    e.preventDefault();
+    socket.emit('addTypingFeedback');
+    
+});
+
+input.addEventListener('input', function(e) {
+    e.preventDefault();
+    socket.emit('addTypingFeedback');
+    
+});
+
+input.addEventListener('blur', function(e) {
+    e.preventDefault();
+    socket.emit('removeTypingFeedback');
+});
+
+socket.on('showMessage', (value)=>{
+    displayMessage(value);
+});
+
+socket.on('showTypingFeedback', ()=>{
+    showTyping();
+});
+
+socket.on('hideTypingFeedback', ()=>{
+    hideTyping();
+});
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-import { Server } from "socket.io";
-import express from "express";
 import { createServer } from 'http';
-
-const PORT = 3000;
-const app = express();
-const server = createServer(app);
-const socket = new Server();
-const io = new Server(server);
+import { Server } from "socket.io";
+import express, { response } from "express";
+import OpenAi from 'openai';
 import path from 'path';
 import { fileURLToPath } from "url";
+import 'dotenv/config';
 
+const PORT = process.env.PORT || 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+
+const openai = new OpenAi({
+    apiKey: process.env.OPENAI_API_KEY
+});
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,10 +27,37 @@ io.on('connection', (socket) => {
     })
     socket.on('message', (value)=>{
         console.log(`recieved this message: ${value}`);
+        // we need to add this message to html page
+        // socket.emit('showMessage', messageArr);
+        // try {
+        //     // const response = await openai.chat.completions.create({
+        //     //     messages:[{
+        //     //         "role": "user", "content": `Turn this message into a riddle \n ${value}`
+        //     //     }],
+        //     //     model: "gpt-3.5-turbo",
+        //     // });
+        //     const response = await openai.chat.completions.create({
+        //         messages:[{
+        //             "role": "user", "content": 'what is 1+89'
+        //         }],
+        //         model: "gpt-3.5-turbo-instruct",
+        //     });
+        //     socket.broadcast.emit('showMessage', response.data.choices[0].text);
+        // } catch (err) {
+        //     console.log(`This is the error from thingy ${err}`);        
+        // }
+        socket.broadcast.emit('showMessage', value);
+    });
+    socket.on('addTypingFeedback', ()=>{
+        socket.broadcast.emit('showTypingFeedback');
+    });
+    socket.on('removeTypingFeedback', ()=>{
+        socket.broadcast.emit('hideTypingFeedback');
     });
 });
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(__dirname + '/index.html');
+
 })
 
 server.listen(3000, () => {
